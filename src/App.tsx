fe80f2aa379c8148b9abdcc95e19e47436dfa367
Jeffreasy <laventejeffrey@gmail.com>
2025-02-18 14:34:17 +0100

    Beschrijf hier je wijzigingen

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,37 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import { ErrorBoundary } from './components/ErrorBoundary'
-import { routes } from './routes'
-
-const router = createBrowserRouter(routes)
+import { Routes, Route } from 'react-router-dom'
+import { LoginPage } from './pages/LoginPage'
+import { DashboardPage } from './pages/DashboardPage'
+import { PhotoManagementPage } from './pages/PhotoManagementPage'
+import { AlbumManagementPage } from './pages/AlbumManagementPage'
+import { VideoManagementPage } from './pages/VideoManagementPage'
+import { PartnerManagementPage } from './pages/PartnerManagementPage'
+import { SponsorManagementPage } from './pages/SponsorManagementPage'
+import { ProfilePage } from './pages/ProfilePage'
+import { NotFoundPage } from './pages/NotFoundPage'
+import { AuthGuard } from './components/auth/AuthGuard'
+import { MainLayout } from './components/layout/MainLayout'
 
 export function App() {
   return (
-    <ErrorBoundary>
-      <RouterProvider router={router} />
-    </ErrorBoundary>
+    <Routes>
+      <Route path="/login" element={<LoginPage />} />
+      <Route
+        element={
+          <AuthGuard>
+            <MainLayout />
+          </AuthGuard>
+        }
+      >
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/photos" element={<PhotoManagementPage />} />
+        <Route path="/albums" element={<AlbumManagementPage />} />
+        <Route path="/videos" element={<VideoManagementPage />} />
+        <Route path="/partners" element={<PartnerManagementPage />} />
+        <Route path="/sponsors" element={<SponsorManagementPage />} />
+        <Route path="/profile" element={<ProfilePage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   )
 } 
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AuthProvider } from './contexts/auth/AuthProvider'
 import { App } from './App'
 import './index.css'
 import './styles/scrollbars.css'
-import { AuthProvider } from './contexts/auth/AuthProvider'
 
 const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </QueryClientProvider>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </QueryClientProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
